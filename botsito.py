4f2054ae01c0b9f1346853f230db4930cd3de915
David Leyton <101845454+devleyton@users.noreply.github.com>
2023-06-11 08:33:19 -0500

    coments

--- botsito.py
+++ botsito.py
@@ -1,6 +1,21 @@
 
-#Importar librerias
-import pywhatkit, time
+#Import frameworks
+import pyautogui, time
 
-#Esperar 5 segundos pa iniciar
+#Wait 5s
 time.sleep (5)
+
+#open execute
+pyautogui.press(win+r)
+
+#open brave (you can change this for other edge, chrome, mozilla)
+pyautogui.write(brave)
+
+#write and open whatsapp web
+pyautogui.write(https://web.whatsapp.com/)
+time.sleep(2)
+pyautogui.press(enter)
+
+#select the first chat
+
+
